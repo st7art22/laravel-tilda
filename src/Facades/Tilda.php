364ab922d181ca2356d3cd126be2f690b7a4e6ea
Andrey Felinger <akaincore@gmail.com>
2018-02-26 02:12:26 +0700

    update phpdoc for tilda facade

--- src/Facades/Tilda.php
+++ src/Facades/Tilda.php
@@ -3,22 +3,26 @@
 namespace IncOre\Tilda\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use IncOre\Tilda\TildaLoader;
-use IncOre\Tilda\TildaApi;
+use IncOre\Tilda\Objects\Page\ExportedPage;
+use IncOre\Tilda\Objects\Page\Page;
+use IncOre\Tilda\Objects\Page\PagesListItem;
+use IncOre\Tilda\Objects\Project\ExportedProject;
+use IncOre\Tilda\Objects\Project\Project;
+use IncOre\Tilda\Objects\Project\ProjectsListItem;
 
 /**
  * Class Tilda
  * @package IncOre\Tilda\Facades
- * @method static TildaLoader page
- * @method static TildaLoader assets
- * @method static TildaApi getProjectsList
- * @method static TildaApi getProject
- * @method static TildaApi getProjectExport
- * @method static TildaApi getPagesList
- * @method static TildaApi getPage
- * @method static TildaApi getPageFull
- * @method static TildaApi getPageExport
- * @method static TildaApi getPageFullExport
+ * @method static ExportedPage page(int $pageId)
+ * @method static array assets(ExportedPage $page)
+ * @method static ProjectsListItem[] getProjectsList
+ * @method static Project getProject(int $projectId, bool $asJson)
+ * @method static ExportedProject getProjectExport(int $projectId, bool $asJson)
+ * @method static PagesListItem[] getPagesList(int $projectId, bool $asJson)
+ * @method static Page getPage(int $pageId, bool $asJson)
+ * @method static Page getPageFull(int $pageId, bool $asJson)
+ * @method static ExportedPage getPageExport(int $pageId, bool $asJson)
+ * @method static ExportedPage getPageFullExport(int $pageId, bool $asJson)
  */
 class Tilda extends Facade
 {
