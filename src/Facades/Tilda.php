188ba0d5a92b3c764c9770225976c52ce704b75c
Andrey Felinger <akaincore@gmail.com>
2018-02-26 01:48:12 +0700

    phpdoc for tilda facade

--- src/Facades/Tilda.php
+++ src/Facades/Tilda.php
@@ -3,7 +3,23 @@
 namespace IncOre\Tilda\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use IncOre\Tilda\TildaLoader;
+use IncOre\Tilda\TildaApi;
 
+/**
+ * Class Tilda
+ * @package IncOre\Tilda\Facades
+ * @method TildaLoader page
+ * @method TildaLoader assets
+ * @method TildaApi getProjectsList
+ * @method TildaApi getProject
+ * @method TildaApi getProjectExport
+ * @method TildaApi getPagesList
+ * @method TildaApi getPage
+ * @method TildaApi getPageFull
+ * @method TildaApi getPageExport
+ * @method TildaApi getPageFullExport
+ */
 class Tilda extends Facade
 {
 
